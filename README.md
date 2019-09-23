84051ef2289c619953410f0af47312f49bd1c61a
Zhangwei Wu <zhangweiwu@outlook.com>
2019-09-22 17:33:55 -0700

    fix

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# powershell-download-arlo
-powershell script to download arlo library
+# ps-download-arlo
+powershell scripts to download arlo library to local
+
+how to run: ps> .\downloadarlo.ps1 -UserName "<email address of arlo account>" -Password "<password of arlo account>" -BaseDir "<local disk root dir, eg. d:\arlo>" -TraceBackDays "<trace back days, from 0 to 6>"
+
+or in cmd: > powershell.exe -Command ".\downloadarlo.ps1" -UserName "<email address of arlo account>" -Password "<password of arlo account>" -BaseDir "<local disk root dir, eg. d:\arlo>" -TraceBackDays "<trace back days, from 0 to 6>"
+
+how to sign your ps script: https://community.spiceworks.com/how_to/153255-windows-10-signing-a-powershell-script-with-a-self-signed-certificate
---
+++ downloadarlo.ps1
@@ -0,0 +1,104 @@
+﻿param([string]$UserName, [string]$Password, [string]$BaseDir, [int]$TraceBackDays)
+
+function GetArloAccessToken
+{
+    param ([string]$userName, [string]$password)
+    
+    $request = "{`"email`":`"$userName`",`"password`":`"$([System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($password)))`",`"language`":`"en`",`"EnvSource`":`"prod`"}"
+    
+    $response = Invoke-RestMethod -Method Post -Uri "https://ocapi-app.arlo.com/api/auth" -Body $request -ContentType "application/json"
+
+    if($? -and $response.meta.code -eq 200)
+    {
+        return $response.data.token
+    }
+
+    $response | ConvertTo-Json | Write-Host
+    return $null
+}
+
+function GetArloLibrary
+{
+    param([string]$token, [string]$datefrom, [string]$dateto)
+
+    $headers = @{
+        "authorization" = $token
+        "auth-version" = "2"
+    }
+
+    $request = "{`"dateFrom`":`"$datefrom`",`"dateTo`":`"$dateto`"}"
+
+    $response = Invoke-RestMethod -Method Post -Uri "https://my.arlo.com/hmsweb/users/library" -Body $request -ContentType "application/json" -Headers $headers
+
+    if($? -and $response.success -eq $true)
+    {
+        return $response.data
+    }
+
+    $response | ConvertTo-Json | Write-Host
+    return $null
+}
+
+function GetSaveFileName
+{
+    param([string]$baseLocation, [string]$createdDate, [string]$deviceId, [string]$name, [string]$reason, [string]$contentType)
+
+    $ext = "unknown"
+    if($contentType -eq "video/mp4")
+    {
+        $ext = "mp4"
+    }
+
+    return "$baseLocation\$($createdDate)\$($reason.ToUpperInvariant())_$($deviceId)_$($name).$ext"
+}
+
+function DownloadArloRecords
+{
+    param([string]$userName, [string]$password, [string]$baseLocation, [int]$traceBackDays)
+
+    $token = GetArloAccessToken $userName $password
+
+    if($token -eq $null)
+    {
+        Write-Error "Failed to Get AccessToken"
+        return
+    }
+
+    if($traceBackDays -ge 6)
+    {
+        $traceBackDays = 6
+    }
+
+    $toDate = Get-Date
+    $fromDate = $toDate.AddDays(-$traceBackDays)
+    
+    $library = GetArloLibrary $token ($fromDate.ToString("yyyyMMdd")) ($toDate.ToString("yyyyMMdd"))
+
+    if($library -eq $null)
+    {
+        Write-Error "Failed to Get Library"
+        return
+    }
+
+    foreach($record in $library)
+    {
+        $fileName = GetSaveFileName $baseLocation $record.createdDate $record.deviceId $record.name $record.reason $record.contentType
+        Write-Host $fileName
+
+        if(Test-Path $fileName)
+        {
+            continue
+        }
+
+        $dir = [System.IO.Path]::GetDirectoryName($fileName)
+        if(-not (Test-Path $dir)) 
+        {
+            New-Item $dir -ItemType Directory -Force
+        }
+
+        Write-Host $record.presignedContentUrl
+        Invoke-WebRequest $record.presignedContentUrl -OutFile $fileName
+    }
+}
+
+DownloadArloRecords $UserName $Password $BaseDir $TraceBackDays
